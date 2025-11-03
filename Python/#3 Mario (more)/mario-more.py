c34766b4bfa7cbc18ef8164f2a31c37dad5b6818
Sergi (Andrew) <sergi.miranda.benitez@gmail.com>
2025-11-03 11:57:56 +0000

    Add files via upload

---
+++ Python/#3 Mario (more)/mario-more.py
@@ -0,0 +1,18 @@
+from cs50 import get_int
+
+# Loops forever until height given is within range
+while True:
+    height = get_int('Height: ')
+    if height in range(1, 9):
+        break
+
+# Prints spaces and blocks
+for i in range(height):
+    print(' ' * (height - i - 1), end='')
+    print('#' * (i + 1), end='')
+
+    # Gap of 2 spaces
+    print(' ' * 2, end='')
+
+    # Blocks on right-hand side
+    print('#' * (i + 1))
